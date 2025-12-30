36df88aab58f58b1b1ce5abdea32bdfe6c02f37e
Anthony Bouch <anthony@infonomic.io>
2025-12-31 03:14:23 +0700

    feat: fixup theme switch (icons updated from uikit)

--- apps/webapp/src/ui/theme/theme-switch.tsx
+++ apps/webapp/src/ui/theme/theme-switch.tsx
@@ -48,10 +48,10 @@ const ThemeSwitch = ({
       <div className="relative w-[24px] h-[24px] flex items-center justify-center">
         {/* Both icons are always rendered; CSS handles visibility to avoid hydration mismatches */}
         <div className="light">
-          <LightIcon svgClassName={lightIconClassName} />
+          <LightIcon svgClassName="text-white" height="22px" width="22px" />
         </div>
         <div className="moon">
-          <MoonIcon svgClassName={moonIconClassName} height="22px" width="22px" />
+          <MoonIcon svgClassName="text-black" height="22px" width="22px" />
         </div>
       </div>
     </div>
